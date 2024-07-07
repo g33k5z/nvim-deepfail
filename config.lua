f70bfead8678a06334e755d01c7426d1e2957f7d
aA <g33k5z@gmail.com>
2024-07-07 10:40:55 -0500

    gd htmldjango filetype

--- config.lua
+++ config.lua
@@ -192,6 +192,15 @@ formatters.setup { { name = "black" }, }
 --     cmd = "TroubleToggle",
 --   },
 -- }
+
+
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, {  "html" })
+local opts = {
+  filetypes = { "html", "htmldjango" }
+}
+require("lvim.lsp.manager").setup("html", opts)
+
+
 lvim.plugins = {
   {
     "folke/trouble.nvim",
