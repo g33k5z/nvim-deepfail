ebb773d4ae1d5ab688de1e3fcf4be85e2068032e
aA <atoledo@platinumids.com>
2024-10-18 10:54:26 -0500

    folke/trouble which-key mapping

--- which-key.lua
+++ which-key.lua
@@ -322,6 +322,16 @@ M.config = function()
         c = { "<cmd>Co<CR>", "Copilot" },
         --TODO: how the fuck do I remap copilot in whichkey
       },
+      t = {
+        name = "Trouble",
+        x = { "<cmd>Trouble diagnostics toggle<cr>", "Diagnostics (Trouble)" },
+        X = { "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Buffer Diagnostics (Trouble)" },
+        s = { "<cmd>Trouble symbols toggle focus=false<cr>", "Symbols (Trouble)" },
+        l = { "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", "LSP Definitions / References / ... (Trouble)" },
+        L = { "<cmd>Trouble loclist toggle<cr>", "Location List (Trouble)" },
+        Q = { "<cmd>Trouble qflist toggle<cr>", "Quickfix List (Trouble)" },
+      },
+
     },
   }
 end
