318f446a984d87b3484c3605db6981bbc3216317
aA <g33k5z@mac.lan>
2025-05-11 04:39:08 -0500

    git update

--- nvim/lua/keymap/which-key/groups/g-git.lua
+++ nvim/lua/keymap/which-key/groups/g-git.lua
@@ -1,7 +1,7 @@
 return {
   { "<leader>g", group = "Git" }, -- Group node
 
-  { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Lazygit" },
+  { "<leader>gg", "<cmd>LazyGitCurrentFile<cr>", desc = "Lazygit" },
   { "<leader>gj", "<cmd>lua require 'gitsigns'.next_hunk({navigation_message = false})<cr>", desc = "Next Hunk" },
   { "<leader>gk", "<cmd>lua require 'gitsigns'.prev_hunk({navigation_message = false})<cr>", desc = "Prev Hunk" },
   { "<leader>gl", "<cmd>lua require 'gitsigns'.blame_line()<cr>", desc = "Blame" },
