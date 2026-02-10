b4d091ffc3c7cd22e9c3153f22abc5fedf295db4
aA <atoledo@platinumids.com>
2026-02-10 13:30:29 -0600

    conpletion styling

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -2,7 +2,7 @@ return {
   -- Completion engine
   {
     "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
+    lazy = false,
     dependencies = {
       "hrsh7th/cmp-nvim-lsp", -- LSP source for nvim-cmp
       "hrsh7th/cmp-buffer",   -- Buffer source for nvim-cmp
@@ -10,15 +10,45 @@ return {
       "hrsh7th/cmp-cmdline",  -- Cmdline source for nvim-cmp
       "L3MON4D3/LuaSnip",     -- Snippet engine
       "saadparwaiz1/cmp_luasnip", -- Snippet source for nvim-cmp
+      "onsails/lspkind.nvim", -- VSCode-like pictograms
     },
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
 
       cmp.setup({
         snippet = {
           expand = function(args)
             luasnip.lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered({
+            side_padding = 1,
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          }),
+          documentation = cmp.config.window.bordered({
+            border = "shadow",
+            winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+          }),
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            -- Set the icon
+            vim_item.kind = string.format(" %s ", lspkind.presets.default[vim_item.kind] or "")
+
+            -- Source mapping
+            local source_names = {
+              nvim_lsp = "LSP",
+              luasnip = "SNIP",
+              buffer = "BUF",
+              path = "PATH",
+            }
+            vim_item.menu = " (" .. (source_names[entry.source.name] or entry.source.name) .. ")"
+
+            return vim_item
           end,
         },
         mapping = cmp.mapping.preset.insert({
