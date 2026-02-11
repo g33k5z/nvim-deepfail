0a41668d9281200c891163d25d2e1724e0d55100
aA <atoledo@platinumids.com>
2026-02-11 10:43:44 -0600

    telescope: prefix/caret bugfix

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -58,8 +58,8 @@ return {
 			local telescope = require("telescope")
 			telescope.setup({
 				defaults = {
-					prompt_prefix = " ",
-					selection_caret = " ",
+					prompt_prefix = "  ",
+					selection_caret = "  ",
 					path_display = { "smart" },
 				},
 				extensions = {
