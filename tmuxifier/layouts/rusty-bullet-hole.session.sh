e07fbecd47ce3e466854b040cdfca7d3ea4262fc
aA <atoledo@platinumids.com>
2026-03-04 13:28:14 -0600

    rbh, misc

--- tmuxifier/layouts/rusty-bullet-hole.session.sh
+++ tmuxifier/layouts/rusty-bullet-hole.session.sh
@@ -1,6 +1,6 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-project="tracker-workspace"
+project="rusty-bullet-hole"
 tmuxifier_templates="$(eval echo ~/.tmux/plugins/tmuxifier/layouts)"
 session_root "'$(eval echo $DEV_ROOT)'/${project}/"
 
@@ -15,44 +15,30 @@ if initialize_session ${project}; then
   select_pane 0
 
   # python venv
-  run_cmd "source env/${project}/bin/activate || mkdir -p env && python3.12 -m venv env/${project} && source env/${project}/bin/activate"
   run_cmd "nvim"
   
   # Wait for nvim to fully load, then send <Space>e, Ctrl-l, and r
   sleep 1  # Increase wait time if nvim takes longer to load
   send_keys "r"
 
-  # Set up the second pane with a horizontal split and run services
+  # Set up the second pane with a horizontal split and run commands in `nu`
   select_pane 1
   run_cmd "opencode"
 
-  split_h 66
+  split_h 40
   select_pane 2
-  # "dev:web": "cd packages/tracker-web && npm run dev",
-  run_cmd "npm run dev:web"
-
-  split_v 50
-  select_pane 3
-  # python venv
-  run_cmd "source env/${project}/bin/activate || mkdir -p env && python3.12 -m venv env/${project} && source env/${project}/bin/activate"
-  #  "dev:api": "cd packages/tracker-api && poetry run fastapi dev app/main.py",
-  run_cmd "npm run dev:api"
-
-  split_v 50
-  select_pane 4
-  #  "dev:analytics": "cd packages/pids-analytics && npm run dev",
-  run_cmd "npm run dev:analytics"
-
+  run_cmd "cargo check"
 
   # Set up a new "nvim-deepfail" window and run commands
   new_window "config"
   select_window "config"
   select_pane 0
   run_cmd "cd '$(eval echo $DEV_ROOT)'/ && nvim ${tmuxifier_templates}/${project}.session.sh"
-
-  split_v 30
-  split_h 40
   
+  # Wait for nvim to load, then send <Space>e, Ctrl-l, and r
+  sleep 2  # Increase wait time if needed
+  send_keys " e"
+
   # Return to "config" window and select pane 0
   select_window "${project}"
   select_pane 0
