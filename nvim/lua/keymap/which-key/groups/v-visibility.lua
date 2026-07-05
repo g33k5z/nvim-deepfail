4a4c0fc7604ba6f497a123a726c5da08f83bf6e9
aA <atoledo@platinumids.com>
2026-07-05 07:33:18 -0500

    whichkey additions | latex,markdown toggles

--- nvim/lua/keymap/which-key/groups/v-visibility.lua
+++ nvim/lua/keymap/which-key/groups/v-visibility.lua
@@ -5,5 +5,30 @@ return {
 		"<cmd>Commentless toggle<cr>",
 		desc = "Toggle Comments",
 	},
+	{ "<leader>vm", group = "Markdown" },
+	{
+		"<leader>vmt",
+		"<cmd>RenderMarkdown toggle<cr>",
+		desc = "Toggle Markdown UI",
+	},
+	{ "<leader>vl", group = "LateX" },
+	{
+		"<leader>vlt",
+		"<cmd>RenderLatex toggle<cr>",
+		desc = "Toggle Latex UI",
+	},
+	{
+		"<leader>vlc",
+		function()
+			local ok, render_latex = pcall(require, "render-latex")
+			if ok then
+				render_latex.clear_all()
+				vim.notify("LaTeX Image Canvas Cleared", vim.log.levels.INFO)
+			else
+				vim.notify("render-latex.nvim not loaded", vim.log.levels.WARN)
+			end
+		end,
+		desc = "Clear LaTeX Images",
+	},
 	-- TODO: Add other visibility toggles (line numbers, indent guides, wrap, etc.)
 }
